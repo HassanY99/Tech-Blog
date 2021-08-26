2ffe0b7c904efb73f04b8c15ffaaae93e58b97c0
Hassan Yusuf <hassanyousuf1999@gmail.com>
2021-08-26 00:09:44 -0400

    public folder created and edited

---
+++ public/js/dashboard.js
@@ -0,0 +1,90 @@
+async function newFormHandler(event) {
+    event.preventDefault();
+  
+    const title = document.querySelector('input[name="post-title"]').value;
+    const post = document.querySelector('input[name="post-content"]').value;
+  
+    const response = await fetch(`/api/posts`, {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+        post,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  
+    if (response.ok) {
+      document.location.replace("/dashboard");
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document
+    .querySelector(".new-post-form")
+    .addEventListener("submit", newFormHandler);
+  
+
+  
+  async function editFormHandler(event) {
+    event.preventDefault();
+  
+    const title = document.querySelector('input[name="post-title"]').value;
+    const post = document.querySelector('input[name="post-content"]').value;
+    const id = window.location.toString().split("/")[
+      window.location.toString().split("/").length - 1
+    ];
+  
+    const response = await fetch(`/api/posts/${id}`, {
+      method: "PUT",
+      body: JSON.stringify({
+        title,
+        post,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  
+    if (response.ok) {
+      document.location.replace("/dashboard/");
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document
+    .querySelector(".edit-post-form")
+    .addEventListener("submit", editFormHandler);
+  
+
+  
+  async function deleteFormHandler(event) {
+    event.preventDefault();
+  
+    const id = window.location.toString().split("/")[
+      window.location.toString().split("/").length - 1
+    ];
+  
+    const response = await fetch(`/api/posts/${id}`, {
+      method: "DELETE",
+      body: JSON.stringify({
+        post_id: id,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  
+    if (response.ok) {
+      document.location.replace("/dashboard/");
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document
+    .querySelector(".delete-post-btn")
+    .addEventListener("click", deleteFormHandler);
